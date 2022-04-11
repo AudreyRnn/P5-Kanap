8795327d5518c4004516bb5854acde30615a3d6f
Audrey Rannou <audrey.rannou@gmail.com>
2022-04-11 17:33:24 +0200

    boucle for of option des couleurs product js

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,6 @@
 let params = new URLSearchParams(window.location.search);
 let id = params.get("id");
+
 console.log(id);
 
 getProduct();
@@ -25,17 +26,30 @@ function getProduct() {
 
 // creation des éléments pour affichage des propriétés des produits
 function getPost(product) {
+
+  // l'image
   let productImg = document.createElement("img");
   document.querySelector(".item__img").appendChild(productImg);
   productImg.src = product.imageUrl;
   productImg.alt = product.altTxt;
 
+  // nom du produit 
   let productName = document.getElementById("title");
   productName.innerHTML = product.name;
 
+  // le prix 
   let productPrice = document.getElementById("price");
   productPrice.innerHTML = product.price;
 
+  // La description 
   let productDescription = document.getElementById("description");
   productDescription.innerHTML = product.description;
+
+  // les options de couleur 
+  for (let colors of product.colors) {
+    let productColors = document.createElement("option");
+    document.querySelector("#colors").appendChild(productColors);
+    productColors.value = colors;
+    productColors.innerHTML = colors;
+  }
 }
